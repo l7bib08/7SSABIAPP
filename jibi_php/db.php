59cc0ba8435ee4c854e36ef3cc0a1735fed74ee0
MOUAD LAHBIB ARBAOUI <mouadlahbibarbaoui08@gmail.com>
2026-04-13 20:58:26 +0100

    PHP connexion file

---
+++ jibi_php/db.php
@@ -0,0 +1,11 @@
+<?php
+
+$host = "localhost";
+$username = "root";
+$password = "";
+$databasename = "gestion_boutique";
+
+
+$conn = new PDO("mysql:host=$host;dbname=$databasename", $username, $password);
+
+?>
